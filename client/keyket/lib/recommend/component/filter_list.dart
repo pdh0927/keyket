3a6f359c1b4328d023ba50ca2f7126cb104c51c3
dh <pdh980927@naver.com>
2023-07-06 23:27:17 +0900

    feat: add filter list component

---
+++ client/keyket/lib/recommend/component/filter_list.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:keyket/common/const/colors.dart';
+import 'package:keyket/recommend/const/text_style.dart';
+import 'package:remixicon/remixicon.dart';
+
+class FilterList extends StatelessWidget {
+  final List<String> featureList;
+  final Function onSelected;
+
+  const FilterList(
+      {super.key, required this.featureList, required this.onSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: featureList.first,
+      icon: const Icon(Remix.arrow_down_s_line),
+      elevation: 16,
+      style: dropdownTextStyle,
+      underline: Container(
+        height: 2,
+        color: BLACK_COLOR,
+      ),
+      onChanged: (String? value) {
+        onSelected(value);
+      },
+      items: featureList.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+            value: value,
+            child: Text(
+              value,
+              style: dropdownTextStyle,
+            ));
+      }).toList(),
+    );
+  }
+}
