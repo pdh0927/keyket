8af83cbf10c99094ea4e794b2910066cf404a68f
dh <pdh980927@naver.com>
2023-07-10 17:56:25 +0900

    chore: move text style to common

--- client/keyket/lib/recommend/const/text_style.dart
+++ client/keyket/lib/recommend/const/text_style.dart
@@ -2,4 +2,3 @@ import 'package:flutter/material.dart';
 import 'package:keyket/common/const/colors.dart';
 
 TextStyle dropdownTextStyle = const TextStyle(fontSize: 16, color: BLACK_COLOR);
-TextStyle bucketTextStyle = const TextStyle(fontSize: 20, color: BLACK_COLOR);
