c012a0fea756fd68028b36b4c4a445484317250a
dh <pdh980927@naver.com>
2023-07-14 21:59:36 +0900

    refactor: add bucket text style file

---
+++ client/keyket/lib/bucket/const/text_style.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:keyket/common/const/colors.dart';
+
+TextStyle toggleButtonTextStlye = const TextStyle(
+    fontSize: 16, fontWeight: FontWeight.w500, color: BLACK_COLOR);
