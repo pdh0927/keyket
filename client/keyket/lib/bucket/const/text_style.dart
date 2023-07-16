7adf8570bc01731a8d73ba1f6de950205d878e06
dh <pdh980927@naver.com>
2023-07-16 16:27:25 +0900

    style: add new text style

--- client/keyket/lib/bucket/const/text_style.dart
+++ client/keyket/lib/bucket/const/text_style.dart
@@ -3,3 +3,6 @@ import 'package:keyket/common/const/colors.dart';
 
 TextStyle toggleButtonTextStlye = const TextStyle(
     fontSize: 16, fontWeight: FontWeight.w500, color: BLACK_COLOR);
+
+TextStyle popupMenuTextStlye = const TextStyle(
+    fontSize: 16, fontWeight: FontWeight.w200, color: BLACK_COLOR);
