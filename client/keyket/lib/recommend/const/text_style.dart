9d275fce4a2e5e987734f637e5c9a2142706f8ef
dh <pdh980927@naver.com>
2023-07-19 23:37:56 +0900

    style: add option text style

--- client/keyket/lib/recommend/const/text_style.dart
+++ client/keyket/lib/recommend/const/text_style.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:keyket/common/const/colors.dart';
 
 TextStyle dropdownTextStyle = const TextStyle(fontSize: 16, color: BLACK_COLOR);
+TextStyle optionTextStyle = const TextStyle(fontSize: 12, color: BLACK_COLOR);
