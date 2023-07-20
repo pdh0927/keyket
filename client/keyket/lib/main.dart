3aa7aedd2df031ba4800f622b8e5a7dd794a8cbb
dh <pdh980927@naver.com>
2023-07-20 13:06:55 +0900

    feat: firebase setting

--- client/keyket/lib/main.dart
+++ client/keyket/lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:keyket/common/view/splash_screen.dart';
+import 'package:keyket/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(_App());
 }
 
