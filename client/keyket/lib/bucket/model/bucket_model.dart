8e1a2d3be99b0e8d432a7cb81e1e6a239f26805a
dh <pdh980927@naver.com>
2023-07-23 13:39:47 +0900

    fix: modify bucket model

--- client/keyket/lib/bucket/model/bucket_model.dart
+++ client/keyket/lib/bucket/model/bucket_model.dart
@@ -1,16 +1,30 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'bucket_model.g.dart';
+
+@JsonSerializable()
 class BucketModel {
   final int id;
   final String name;
   final String image;
-  final double achievement_rate;
-  final DateTime created_at;
-  final DateTime updated_at;
+  final double achievementRate;
+  final bool isShared;
+  final List<String> users;
+  final DateTime createdAt;
+  final DateTime updatedAt;
 
   BucketModel(
       {required this.id,
       required this.name,
       required this.image,
-      required this.achievement_rate,
-      required this.created_at,
-      required this.updated_at});
+      required this.achievementRate,
+      required this.isShared,
+      required this.users,
+      required this.createdAt,
+      required this.updatedAt});
+
+  factory BucketModel.fromJson(Map<String, dynamic> json) =>
+      _$BucketModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$BucketModelToJson(this);
 }
