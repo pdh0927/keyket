5ba9a0cbbf96834087eb938fa31089d5c749136d
dh <pdh980927@naver.com>
2023-07-26 01:49:49 +0900

    feat: create bucketListItemProvider

--- client/keyket/lib/bucket/provider/bucket_list_provider.dart
+++ client/keyket/lib/bucket/provider/bucket_list_provider.dart
@@ -2,9 +2,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:keyket/bucket/model/bucket_list_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:keyket/bucket/model/custom_item_model.dart';
+import 'package:keyket/recommend/model/recommend_item_model.dart';
 
 final firestore = FirebaseFirestore.instance;
-Query<Map<String, dynamic>> query = firestore.collection('bucket_list');
 
 final myBucketListListProvider =
     StateNotifierProvider<MyBucketListNotifier, List<BucketListModel>>((ref) {
@@ -13,18 +14,18 @@ final myBucketListListProvider =
 
 class MyBucketListNotifier extends StateNotifier<List<BucketListModel>> {
   MyBucketListNotifier() : super([]) {
-    getMyBucketListd();
+    getMyBucketList();
   }
 
-  void getMyBucketListd() async {
+  void getMyBucketList() async {
     List<BucketListModel> myBucketListList = [];
 
     try {
-      Query<Map<String, dynamic>> myQuery;
-      myQuery = query.where('users', arrayContains: 'dh');
-      myQuery = myQuery.where('isShared', isEqualTo: false);
+      Query<Map<String, dynamic>> query = firestore.collection('bucket_list');
+      query = query.where('users', arrayContains: 'dh');
+      query = query.where('isShared', isEqualTo: false);
 
-      QuerySnapshot<Map<String, dynamic>> docList = await myQuery.get();
+      QuerySnapshot<Map<String, dynamic>> docList = await query.get();
 
       for (var doc in docList.docs) {
         Map<String, dynamic> data = _docToMap(doc);
@@ -69,11 +70,11 @@ class SharedBucketListNotifier extends StateNotifier<List<BucketListModel>> {
     List<BucketListModel> sharedBucketListList = [];
 
     try {
-      Query<Map<String, dynamic>> sharedQuery;
-      sharedQuery = query.where('users', arrayContains: 'dh');
-      sharedQuery = sharedQuery.where('isShared', isEqualTo: true);
+      Query<Map<String, dynamic>> query = firestore.collection('bucket_list');
+      query = query.where('users', arrayContains: 'dh');
+      query = query.where('isShared', isEqualTo: true);
 
-      QuerySnapshot<Map<String, dynamic>> docList = await sharedQuery.get();
+      QuerySnapshot<Map<String, dynamic>> docList = await query.get();
 
       for (var doc in docList.docs) {
         Map<String, dynamic> data = _docToMap(doc);
@@ -123,4 +124,70 @@ Map<String, dynamic> _docToMap(
   data['updatedAt'] = updatedAtString;
 
   return data;
+}
+
+final bucketListItemProvider =
+    StateNotifierProvider<BucketListItemNotifier, Map<String, dynamic>>((ref) {
+  return BucketListItemNotifier();
+});
+
+class BucketListItemNotifier extends StateNotifier<Map<String, dynamic>> {
+  BucketListItemNotifier() : super({}) {}
+
+  void getItems(String id, List<String> customItemList,
+      List<String> recommendItemList) async {
+    List<Item> itemList = [];
+
+    try {
+      // 10개 단위로 나누기
+      List<List<String>> customItemChunks = [];
+      for (int i = 0; i < customItemList.length; i += 10) {
+        customItemChunks.add(customItemList.sublist(i,
+            i + 10 > customItemList.length ? customItemList.length : i + 10));
+      }
+
+      // 각 10개 단위의 부분 리스트에 대해 쿼리를 실행
+      for (List<String> chunk in customItemChunks) {
+        QuerySnapshot querySnapshot = await firestore
+            .collection('custom')
+            .where(FieldPath.documentId, whereIn: chunk)
+            .get();
+        for (DocumentSnapshot doc in querySnapshot.docs) {
+          Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+          data['id'] = doc.id;
+          itemList.add(CustomItemModel.fromJson(data));
+        }
+      }
+
+      // 10개 단위로 나누기
+      List<List<String>> recommendItemChunks = [];
+      for (int i = 0; i < recommendItemList.length; i += 10) {
+        recommendItemChunks.add(recommendItemList.sublist(
+            i,
+            i + 10 > recommendItemList.length
+                ? recommendItemList.length
+                : i + 10));
+      }
+
+      // 각 10개 단위의 부분 리스트에 대해 쿼리를 실행
+      for (List<String> chunk in recommendItemChunks) {
+        QuerySnapshot querySnapshot = await firestore
+            .collection('recommend')
+            .where(FieldPath.documentId, whereIn: chunk)
+            .get();
+        for (DocumentSnapshot doc in querySnapshot.docs) {
+          Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+          data['id'] = doc.id;
+
+          itemList.add(RecommendItemModel.fromJson(data));
+        }
+      }
+
+      // 상태 갱신
+      state = {...state, id: itemList};
+      print(state);
+    } catch (e) {
+      print(e);
+    }
+  }
 }
