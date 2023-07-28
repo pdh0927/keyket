b823b7f719f4169bbd3ea92cc39e0ff277c52185
dh <pdh980927@naver.com>
2023-07-28 17:27:51 +0900

    feat: create modify and delete button

--- client/keyket/lib/common/component/list_item.dart
+++ client/keyket/lib/common/component/list_item.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:keyket/bucket/model/custom_item_model.dart';
 import 'package:keyket/common/const/colors.dart';
 import 'package:keyket/common/const/text_style.dart';
+import 'package:remixicon/remixicon.dart';
 
 class ListItem extends StatelessWidget {
   const ListItem(
       {super.key,
       required this.selectFlag,
       required this.isContain,
+      required this.isRecommendItem,
       required this.onPressed,
-      required this.content});
+      this.removeItem,
+      this.modifyItem,
+      required this.item});
   final bool selectFlag;
   final bool isContain;
+  final bool isRecommendItem;
   final Function() onPressed;
-  final String content;
+  final Function? removeItem;
+  final Function? modifyItem;
+  final ItemModel item;
 
   @override
   Widget build(BuildContext context) {
+    // IconButton에 대한 별도의 GlobalKey를 생성합니다.
+    final buttonKey = GlobalKey();
+
     return Row(
       children: [
         selectFlag == true
@@ -43,15 +54,128 @@ class ListItem extends StatelessWidget {
         Expanded(
           child: Column(
             children: [
-              Container(
-                alignment: Alignment.centerLeft,
-                height: 55,
-                padding: EdgeInsets.only(left: selectFlag == true ? 0 : 10),
-                child: Text(
-                  content,
-                  style: dropdownTextStyle,
-                  textAlign: TextAlign.start,
-                ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      height: 55,
+                      padding:
+                          EdgeInsets.only(left: selectFlag == true ? 0 : 10),
+                      child: Text(
+                        item.content,
+                        style: dropdownTextStyle,
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                  ),
+                  !isRecommendItem
+                      ? IconButton(
+                          key: buttonKey, // IconButton에 GlobalKey를 연결합니다.
+                          padding: const EdgeInsets.all(0),
+                          splashRadius: 15,
+                          constraints:
+                              const BoxConstraints(maxWidth: 35, minWidth: 35),
+                          onPressed: () {
+                            // 버튼의 위치와 크기를 얻습니다.
+                            final RenderBox renderBox =
+                                buttonKey.currentContext!.findRenderObject()
+                                    as RenderBox;
+                            final size = renderBox.size;
+                            final position =
+                                renderBox.localToGlobal(Offset.zero);
+
+                            // IconButton의 아래에 오버레이를 표시합니다.
+                            late OverlayEntry overlayEntry;
+                            overlayEntry = OverlayEntry(
+                              builder: (context) => Stack(
+                                children: <Widget>[
+                                  // 외부를 클릭하면 오버레이를 제거하는 GestureDetector
+                                  Positioned.fill(
+                                    child: GestureDetector(
+                                      behavior: HitTestBehavior.opaque,
+                                      onTap: () => overlayEntry.remove(),
+                                      child: Container(),
+                                    ),
+                                  ),
+
+                                  // 오버레이 내용
+                                  Positioned(
+                                    top: position.dy + size.height - 10,
+                                    left: position.dx - 95,
+                                    child: Material(
+                                      child: Row(
+                                        children: <Widget>[
+                                          TextButton(
+                                            style: TextButton.styleFrom(
+                                              backgroundColor:
+                                                  GREY_COLOR.withOpacity(0.2),
+                                              padding: EdgeInsets.zero,
+                                              shape:
+                                                  const RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.only(
+                                                  topLeft: Radius.circular(10),
+                                                  bottomLeft:
+                                                      Radius.circular(10),
+                                                ),
+                                              ),
+                                            ),
+                                            onPressed: () {
+                                              overlayEntry
+                                                  .remove(); // 버튼을 클릭하면 오버레이를 제거
+                                            },
+                                            child: const Text(
+                                              '수정하기',
+                                              style: TextStyle(
+                                                  fontFamily: 'SCDream',
+                                                  fontSize: 10),
+                                            ),
+                                          ),
+                                          TextButton(
+                                            style: TextButton.styleFrom(
+                                              backgroundColor:
+                                                  GREY_COLOR.withOpacity(0.2),
+                                              padding: EdgeInsets.zero,
+                                              shape:
+                                                  const RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.only(
+                                                  topRight: Radius.circular(10),
+                                                  bottomRight:
+                                                      Radius.circular(10),
+                                                ),
+                                              ),
+                                            ),
+                                            onPressed: () {
+                                              overlayEntry
+                                                  .remove(); // 버튼을 클릭하면 오버레이를 제거
+                                              removeItem!(item);
+                                            },
+                                            child: const Text(
+                                              '삭제하기',
+                                              style: TextStyle(
+                                                  fontFamily: 'SCDream',
+                                                  fontSize: 10),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                            Overlay.of(context).insert(overlayEntry);
+                          },
+                          icon: const Icon(
+                            Remix.more_line,
+                            size: 20,
+                            color: Color(0xFF616161),
+                          ),
+                        )
+                      : const SizedBox(
+                          width: 0,
+                        ),
+                ],
               ),
               const Divider(
                 // 구분선
