7b7927e35b4071ef37eb942504ca63e9e34aba13
dh <pdh980927@naver.com>
2023-08-03 17:59:07 +0900

    fix: fix  kakao login exception

--- client/keyket/lib/common/view/login_screen.dart
+++ client/keyket/lib/common/view/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:keyket/common/layout/default_layout.dart';
 
@@ -41,14 +42,43 @@ Widget _loginButton(String path, VoidCallback onTap) {
 }
 
 Future<void> onTapFuture(BuildContext context) async {
-  bool isInstalled = await isKakaoTalkInstalled();
-  OAuthToken token = isInstalled
-      ? await UserApi.instance.loginWithKakaoTalk()
-      : await UserApi.instance.loginWithKakaoAccount();
-  // TODO: Handle the token
-  print(token);
+  // 카카오톡 실행 가능 여부 확인
+  // 카카오톡 실행이 가능하면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+  if (await isKakaoTalkInstalled()) {
+    try {
+      await UserApi.instance.loginWithKakaoTalk();
+      print('카카오톡으로 로그인 성공');
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (_) => const SplashScreen()),
+          (route) => false);
+    } catch (error) {
+      print('카카오톡으로 로그인 실패 $error');
 
-  Navigator.of(context).pushAndRemoveUntil(
-      MaterialPageRoute(builder: (_) => const SplashScreen()),
-      (route) => false);
+      // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+      // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+      if (error is PlatformException && error.code == 'CANCELED') {
+        return;
+      }
+      // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+      try {
+        await UserApi.instance.loginWithKakaoAccount();
+        print('카카오계정으로 로그인 성공');
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (_) => const SplashScreen()),
+            (route) => false);
+      } catch (error) {
+        print('카카오계정으로 로그인 실패 $error');
+      }
+    }
+  } else {
+    try {
+      await UserApi.instance.loginWithKakaoAccount();
+      print('카카오계정으로 로그인 성공');
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (_) => const SplashScreen()),
+          (route) => false);
+    } catch (error) {
+      print('카카오계정으로 로그인 실패 $error');
+    }
+  }
 }
