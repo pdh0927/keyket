0f9309b510f8b20236e65895747bd6477e81af6e
dh <pdh980927@naver.com>
2023-08-05 00:57:12 +0900

    feat : firebase authentication

--- client/keyket/lib/common/model/main_view_model.dart
+++ client/keyket/lib/common/model/main_view_model.dart
@@ -1,22 +1,37 @@
-import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart' as kakao;
+import 'package:keyket/common/model/firebase_auth_remote_data_source.dart';
 import 'package:keyket/common/model/social_login.dart';
 
 class MainViewModel {
+  final _firebaseAuthDataSource = FirebaseAuthRemoteDataSource();
   final SocialLogin _socialLogin;
   bool isLogin = false;
-  User? user;
+  kakao.User? user;
 
   MainViewModel(this._socialLogin);
 
-  Future login() async {
-    isLogin == await _socialLogin.login();
+  Future<bool> login() async {
+    isLogin = await _socialLogin.login();
+
     if (isLogin) {
-      user = await UserApi.instance.me();
+      user = await kakao.UserApi.instance.me();
+      final customToken = await _firebaseAuthDataSource.createCustomUserToken({
+        'uid': user!.id.toString(),
+        'displayname': user!.kakaoAccount!.profile!.nickname,
+        'photoURL': user!.kakaoAccount!.profile!.profileImageUrl
+      });
+      print(customToken);
+      await FirebaseAuth.instance.signInWithCustomToken(customToken);
+
+      return true;
     }
+    return false;
   }
 
   Future logout() async {
     await _socialLogin.logout();
+    await FirebaseAuth.instance.signOut();
     isLogin = false;
     user = null;
   }
