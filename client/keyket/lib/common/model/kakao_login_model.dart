13d45c55bda058b996c31cecffd6bac9457ceb8b
dh <pdh980927@naver.com>
2023-08-05 15:42:52 +0900

    retactor: move user save function

--- client/keyket/lib/common/model/kakao_login_model.dart
+++ client/keyket/lib/common/model/kakao_login_model.dart
@@ -1,11 +1,6 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:keyket/common/model/social_login.dart';
-import 'package:keyket/common/model/user_model.dart';
 
 class KaKaoLoginModel implements SocialLogin {
   @override
@@ -16,25 +11,9 @@ class KaKaoLoginModel implements SocialLogin {
       if (await isKakaoTalkInstalled()) {
         try {
           OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
-          final url = Uri.https('kapi.kakao.com', '/v2/user/me');
 
-          final response = await http.get(
-            url,
-            headers: {
-              HttpHeaders.authorizationHeader: 'Bearer ${token.accessToken}'
-            },
-          );
-
-          final profileInfo = json.decode(response.body);
-
-          // JSON에서 필요한 정보 추출
-          // JSON에서 필요한 정보 추출
-          String kakaoId = profileInfo['id'].toString();
-          String name = profileInfo['properties']['nickname'];
-          String image = profileInfo['properties']['thumbnail_image'];
-
-          // Firestore에 사용자 데이터 저장
-          await _createUserInFirestore(kakaoId, name, image);
+          // 토큰 저장
+          TokenManagerProvider.instance.manager.setToken(token);
 
           return true;
         } catch (error) {
@@ -48,23 +27,9 @@ class KaKaoLoginModel implements SocialLogin {
           // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
           try {
             OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
-            final url = Uri.https('kapi.kakao.com', '/v2/user/me');
-            final response = await http.get(
-              url,
-              headers: {
-                HttpHeaders.authorizationHeader: 'Bearer ${token.accessToken}'
-              },
-            );
 
-            final profileInfo = json.decode(response.body);
-
-            // JSON에서 필요한 정보 추출
-            String kakaoId = profileInfo['id'].toString();
-            String name = profileInfo['properties']['nickname'];
-            String image = profileInfo['properties']['thumbnail_image'];
-
-            // Firestore에 사용자 데이터 저장
-            await _createUserInFirestore(kakaoId, name, image);
+            // 토큰 저장
+            TokenManagerProvider.instance.manager.setToken(token);
 
             return true;
           } catch (error) {
@@ -79,24 +44,6 @@ class KaKaoLoginModel implements SocialLogin {
 
           // 토큰 저장
           TokenManagerProvider.instance.manager.setToken(token);
-
-          final url = Uri.https('kapi.kakao.com', '/v2/user/me');
-          final response = await http.get(
-            url,
-            headers: {
-              HttpHeaders.authorizationHeader: 'Bearer ${token.accessToken}'
-            },
-          );
-
-          final profileInfo = json.decode(response.body);
-
-          // JSON에서 필요한 정보 추출
-          String kakaoId = profileInfo['id'].toString();
-          String name = profileInfo['properties']['nickname'];
-          String image = profileInfo['properties']['thumbnail_image'];
-
-          // Firestore에 사용자 데이터 저장
-          await _createUserInFirestore(kakaoId, name, image);
 
           return true;
         } catch (error) {
@@ -120,41 +67,4 @@ class KaKaoLoginModel implements SocialLogin {
       return false;
     }
   }
-}
-
-Future<void> _createUserInFirestore(
-    String kakaoId, String name, String image) async {
-  var counterRef =
-      FirebaseFirestore.instance.collection('user_count').doc('userCounter');
-  var userRef = FirebaseFirestore.instance.collection('user');
-
-  DocumentSnapshot userSnapshot = await userRef.doc(kakaoId).get();
-
-  if (userSnapshot.exists) {
-    // 이미 존재하는 사용자인 경우 종료
-    print('User already exists.');
-    return;
-  }
-
-  FirebaseFirestore.instance.runTransaction((transaction) async {
-    DocumentSnapshot counterSnapshot = await transaction.get(counterRef);
-
-    if (!counterSnapshot.exists) {
-      throw Exception('User counter does not exist');
-    } else {
-      Map<String, dynamic> counterData =
-          counterSnapshot.data() as Map<String, dynamic>;
-      int updatedCount = counterData['count'] + 1;
-      transaction.update(counterRef, {'count': updatedCount});
-
-      UserModel user = UserModel(
-        kakaoId: kakaoId,
-        nickname: name,
-        image: image,
-        inviteCode: updatedCount,
-      );
-
-      transaction.set(userRef.doc(kakaoId), user.toJson());
-    }
-  });
 }
--- client/keyket/lib/common/model/main_view_model.dart
+++ client/keyket/lib/common/model/main_view_model.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart' as kakao;
 import 'package:keyket/common/model/firebase_auth_remote_data_source.dart';
 import 'package:keyket/common/model/social_login.dart';
+import 'package:keyket/common/model/user_model.dart';
 
 class MainViewModel {
   final _firebaseAuthDataSource = FirebaseAuthRemoteDataSource();
@@ -19,10 +21,16 @@ class MainViewModel {
       final customToken = await _firebaseAuthDataSource.createCustomUserToken({
         'uid': user!.id.toString(),
         'displayname': user!.kakaoAccount!.profile!.nickname,
-        'photoURL': user!.kakaoAccount!.profile!.profileImageUrl
       });
-      print(customToken);
+
       await FirebaseAuth.instance.signInWithCustomToken(customToken);
+
+      // JSON에서 필요한 정보 추출
+      String kakaoId = user!.id.toString();
+      String name = user!.kakaoAccount!.profile!.nickname!;
+
+      // Firestore에 사용자 데이터 저장
+      await _createUserInFirestore(kakaoId, name);
 
       return true;
     }
@@ -35,4 +43,40 @@ class MainViewModel {
     isLogin = false;
     user = null;
   }
+}
+
+Future<void> _createUserInFirestore(String kakaoId, String name) async {
+  var counterRef =
+      FirebaseFirestore.instance.collection('user_count').doc('userCounter');
+  var userRef = FirebaseFirestore.instance.collection('user');
+
+  DocumentSnapshot userSnapshot = await userRef.doc(kakaoId).get();
+
+  if (userSnapshot.exists) {
+    // 이미 존재하는 사용자인 경우 종료
+    print('User already exists.');
+    return;
+  }
+
+  FirebaseFirestore.instance.runTransaction((transaction) async {
+    DocumentSnapshot counterSnapshot = await transaction.get(counterRef);
+
+    if (!counterSnapshot.exists) {
+      throw Exception('User counter does not exist');
+    } else {
+      Map<String, dynamic> counterData =
+          counterSnapshot.data() as Map<String, dynamic>;
+      int updatedCount = counterData['count'] + 1;
+      transaction.update(counterRef, {'count': updatedCount});
+
+      UserModel user = UserModel(
+        kakaoId: kakaoId,
+        nickname: name,
+        image: '',
+        inviteCode: updatedCount,
+      );
+
+      transaction.set(userRef.doc(kakaoId), user.toJson());
+    }
+  });
 }
