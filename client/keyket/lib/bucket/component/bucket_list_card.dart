b0f2fc20aae6ea7214b83df07d12317a9f4a8aee
dh <pdh980927@naver.com>
2023-08-10 23:20:57 +0900

    fix: bucketlist button function rollback

--- client/keyket/lib/bucket/component/bucket_list_card.dart
+++ client/keyket/lib/bucket/component/bucket_list_card.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:keyket/bucket/component/custom_progressbar.dart';
 import 'package:keyket/bucket/const/text_style.dart';
 import 'package:keyket/bucket/model/bucket_list_model.dart';
+import 'package:keyket/bucket/view/bucket_list_detail_screen.dart';
 import 'package:keyket/common/const/colors.dart';
 
 import 'package:keyket/common/provider/my_provider.dart';
@@ -60,38 +61,47 @@ class BucketListCard extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Stack(
       children: [
-        IntrinsicHeight(
-          child: Row(children: [
-            ClipRRect(borderRadius: BorderRadius.circular(5.0), child: image),
-            const SizedBox(width: 25),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Row(
-                  children: [
-                    const Icon(Remix.arrow_right_s_line),
-                    Text(
-                      name,
-                      style: popupMenuTextStlye,
-                    )
-                  ],
-                ),
-                CustomProgressBar(
-                  achievementRate: getAchievementRate(),
-                  height: 17,
-                  width: 160,
-                ),
-                Text(
-                  '최근 수정 : ${updatedAt.year.toString().substring(2)}.${updatedAt.month.toString().padLeft(2, '0')}.${updatedAt.day.toString().padLeft(2, '0')}',
-                  style: const TextStyle(
-                      fontFamily: 'SCDream',
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400),
-                )
-              ],
-            )
-          ]),
+        InkWell(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => BucketListDetailScreen(
+                      bucketListId: id,
+                      isShared: isShared,
+                    )));
+          },
+          child: IntrinsicHeight(
+            child: Row(children: [
+              ClipRRect(borderRadius: BorderRadius.circular(5.0), child: image),
+              const SizedBox(width: 25),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Remix.arrow_right_s_line),
+                      Text(
+                        name,
+                        style: popupMenuTextStlye,
+                      )
+                    ],
+                  ),
+                  CustomProgressBar(
+                    achievementRate: getAchievementRate(),
+                    height: 17,
+                    width: 160,
+                  ),
+                  Text(
+                    '최근 수정 : ${updatedAt.year.toString().substring(2)}.${updatedAt.month.toString().padLeft(2, '0')}.${updatedAt.day.toString().padLeft(2, '0')}',
+                    style: const TextStyle(
+                        fontFamily: 'SCDream',
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400),
+                  )
+                ],
+              )
+            ]),
+          ),
         ),
         Positioned(
           top: 5,
