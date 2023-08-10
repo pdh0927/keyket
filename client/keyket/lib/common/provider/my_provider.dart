feaa379df76c63f0a6e1f556390f2b4a8609d444
dh <pdh980927@naver.com>
2023-08-10 17:07:00 +0900

    feat: add get user information function

--- client/keyket/lib/common/provider/my_provider.dart
+++ client/keyket/lib/common/provider/my_provider.dart
@@ -1,21 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:keyket/common/model/user_model.dart';
 
 final myInformationProvider =
-    StateNotifierProvider<MyInformationNotifer, User?>((ref) {
+    StateNotifierProvider<MyInformationNotifer, UserModel?>((ref) {
   return MyInformationNotifer();
 });
 
-class MyInformationNotifer extends StateNotifier<User?> {
+class MyInformationNotifer extends StateNotifier<UserModel?> {
   MyInformationNotifer() : super(null) {
-    getMyInformation();
+    loadUserInfo();
   }
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Future<void> loadUserInfo() async {
+    final user = FirebaseAuth.instance.currentUser;
+    print('실행');
 
-  Future getMyInformation() async {
-    User? user = FirebaseAuth.instance.currentUser;
     if (user != null) {
-      state = user;
+      final DocumentSnapshot userDoc =
+          await _firestore.collection('user').doc(user.uid).get();
+
+      if (userDoc.exists) {
+        final data = userDoc.data() as Map<String, dynamic>;
+        data['id'] = userDoc.id;
+        state = UserModel.fromJson(data);
+      }
     }
-    print(user);
   }
 }
