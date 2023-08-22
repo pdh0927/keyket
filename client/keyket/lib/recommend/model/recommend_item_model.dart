bf4a0210acf2c301fd2c443618f05a1675af5c09
dh <pdh980927@naver.com>
2023-08-23 02:14:17 +0900

    chore: synchronize region

--- client/keyket/lib/recommend/model/recommend_item_model.dart
+++ client/keyket/lib/recommend/model/recommend_item_model.dart
@@ -8,41 +8,14 @@ enum RecommendRegion {
   seoul,
   busan,
   daegu,
-  gwanju,
-  incheon,
-  daejeon,
-  ulsan,
-  gyeonggi,
-  gangwon,
-  chungbuk,
-  chungnam,
-  jeonbuk,
-  jeonnam,
-  gyeongbuk,
-  gyeongnam,
-  jeju
+  pohang,
+  gyeongju,
+  jeju,
 }
 
 enum RecommendTheme { healing, activity, festival, date, hist, food }
 
-List<String> recommendRegionKor = [
-  '서울',
-  '부산',
-  '대구',
-  '광주',
-  '인천',
-  '대전',
-  '울산',
-  '경기',
-  '강원',
-  '충북',
-  '충남',
-  '전북',
-  '전남',
-  '경북',
-  '경남',
-  '제주'
-];
+List<String> recommendRegionKor = ['서울', '부산', '대구', '포항', '경주', '제주'];
 
 List<String> recommendThemeKor = ['힐링', '엑티비티', '축제', '데이트', '역사/문화', '먹거리'];
 
