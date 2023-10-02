5a5f12344f20654b70ca696158dc1e22d6bad046
dh <pdh980927@naver.com>
2023-10-02 13:28:37 +0900

    docs

--- lib/tmp/api_test.dart
+++ lib/tmp/api_test.dart
@@ -1,31 +1,38 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'dart:convert';
 
 void apiTest() async {
+  DateTime now = DateTime(2023, 10, 2);
+  int yyyymmdd =
+      int.parse('${now.year}${_twoDigit(now.month)}${_twoDigit(now.day)}');
+
   final Map<String, dynamic> params = {
-    'serviceKey':
+    'ServiceKey':
         'xBDpjEAn5RzvzNhwBgo/BTjxXFd07srl7FzKbHOXh0liVSTWzSEF/8fFK9in+oJNI26MkaHvUyhPQ067LYXuKQ==',
     'pageNo': '1',
-    'numOfRows': '10',
-    'SIDO_NM': '제주',
-    'resultType': 'json'
+    'numOfRows': '1',
+    'dataType': 'json',
+    'CURRENT_DATE': yyyymmdd.toString(),
+    'HOUR': '24',
+    'COURSE_ID': '122'
   };
 
   final Dio dio = Dio();
 
   try {
     Response response = await dio.get(
-      'http://apis.data.go.kr/1192000/service/OceansBeachInfoService1/getOceansBeachInfo1',
+      'http://apis.data.go.kr/1360000/TourStnInfoService1/getTourStnVilageFcst1',
       queryParameters: params,
     );
-    Map<String, dynamic> decodedJson = json.decode(response.data);
-
-    for (int i = 0; i < decodedJson['getOceansBeachInfo']['item'].length; i++) {
-      print(decodedJson['getOceansBeachInfo']['item'][i]);
-    }
-    print(decodedJson['getOceansBeachInfo']['item'].length);
+    // print(response.statusCode);
+    // Map<String, dynamic> decodedJson = json.decode(response.data);
+    // print(decodedJson);
+    print(response.data['response']['body']['items']['item'][0]);
   } catch (e) {
     print(e);
   }
+}
+
+String _twoDigit(int n) {
+  if (n >= 10) return n.toString();
+  return '0$n';
 }
